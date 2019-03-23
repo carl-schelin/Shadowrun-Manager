add7ceb87f483daaccb37ed933c80a2e178aae3e
Carl Schelin <t.carl.schelin@gmail.com>
2019-03-23 03:40:13 +0000

    updated to have it match the edit page; grades and specializations

--- manage/bioware.mysql.php
+++ manage/bioware.mysql.php
@@ -43,7 +43,6 @@
 
   $output .= "</div>";
 
-
   $output .= "<table class=\"ui-styled-table\" width=\"100%\">";
   $output .= "<tr>";
   $output .= "  <th class=\"ui-state-default\">Class</th>";
@@ -55,10 +54,12 @@
   $output .= "  <th class=\"ui-state-default\">Book/Page</th>";
   $output .= "</tr>";
 
-  $nuyen = '&yen;';
-  $q_string  = "select r_bio_id,bio_class,bio_name,bio_rating,bio_essence,bio_avail,bio_perm,ver_book,bio_page,bio_cost ";
+  $q_string  = "select r_bio_id,class_name,bio_class,bio_name,r_bio_specialize,bio_rating,bio_essence,bio_avail,";
+  $q_string .= "bio_perm,ver_book,bio_page,bio_cost,grade_name,grade_essence ";
   $q_string .= "from r_bioware ";
   $q_string .= "left join bioware on bioware.bio_id = r_bioware.r_bio_number ";
+  $q_string .= "left join class on class.class_id = bioware.bio_class ";
+  $q_string .= "left join grades on grades.grade_id = r_bioware.r_bio_grade ";
   $q_string .= "left join versions on versions.ver_id = bioware.bio_book ";
   $q_string .= "where r_bio_character = " . $formVars['id'] . " ";
   $q_string .= "order by bio_class,bio_name,bio_rating,ver_version ";
@@ -66,20 +67,34 @@
   if (mysql_num_rows($q_r_bioware) > 0) {
     while ($a_r_bioware = mysql_fetch_array($q_r_bioware)) {
 
+      $bio_name = $a_r_bioware['bio_name'];
+      if ($a_r_bioware['r_bio_specialize'] != '') {
+        $bio_name = $a_r_bioware['bio_name'] . " (" . $a_r_bioware['r_bio_specialize'] . ")";
+      }
+
+      $grade = '';
+      if ($a_r_bioware['grade_essence'] != 1.00) {
+        $grade = " (" . $a_r_bioware['grade_name'] . ")";
+      }
+
       $rating = return_Rating($a_r_bioware['bio_rating']);
 
       $essence = return_Essence($a_r_bioware['bio_essence']);
 
+      $cost = return_Cost($a_r_bioware['bio_cost']);
+
+      $book = return_Book($a_r_bioware['ver_book'], $a_r_bioware['bio_page']);
+
       $avail = return_Avail($a_r_bioware['bio_avail'], $a_r_bioware['bio_perm']);
 
       $output .= "<tr>";
-      $output .= "<td class=\"ui-widget-content\">"        . $a_r_bioware['bio_class']                                     . "</td>";
-      $output .= "<td class=\"ui-widget-content\">"        . $a_r_bioware['bio_name']                                      . "</td>";
-      $output .= "<td class=\"ui-widget-content delete\">" . $rating                                                       . "</td>";
-      $output .= "<td class=\"ui-widget-content delete\">" . $essence                                                      . "</td>";
-      $output .= "<td class=\"ui-widget-content delete\">" . $avail                                                        . "</td>";
-      $output .= "<td class=\"ui-widget-content delete\">" . number_format($a_r_bioware['bio_cost'], 0, '.', ',') . $nuyen . "</td>";
-      $output .= "<td class=\"ui-widget-content delete\">" . $a_r_bioware['ver_book'] . ": " . $a_r_bioware['bio_page']    . "</td>";
+      $output .= "<td class=\"ui-widget-content\">"        . $a_r_bioware['class_name'] . "</td>";
+      $output .= "<td class=\"ui-widget-content\">"        . $bio_name . $grade         . "</td>";
+      $output .= "<td class=\"ui-widget-content delete\">" . $rating                    . "</td>";
+      $output .= "<td class=\"ui-widget-content delete\">" . $essence                   . "</td>";
+      $output .= "<td class=\"ui-widget-content delete\">" . $avail                     . "</td>";
+      $output .= "<td class=\"ui-widget-content delete\">" . $cost                      . "</td>";
+      $output .= "<td class=\"ui-widget-content delete\">" . $book                      . "</td>";
       $output .= "</tr>";
 
     }
