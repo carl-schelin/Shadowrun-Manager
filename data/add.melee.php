14d688445dd64ef9508a44daf87a64a847ce92f0
Carl Schelin <cschelin@internal.pri>
2023-07-05 02:58:04 +0000

    added the attack rating and removed the dialog box

--- data/add.melee.php
+++ data/add.melee.php
@@ -66,6 +66,11 @@ function attach_melee(p_script_url, update) {
   am_url += "&melee_name="       + encode_URI(am_form.melee_name.value);
   am_url += "&melee_acc="        + encode_URI(am_form.melee_acc.value);
   am_url += "&melee_reach="      + encode_URI(am_form.melee_reach.value);
+  am_url += "&melee_ar1="        + encode_URI(am_form.melee_ar1.value);
+  am_url += "&melee_ar2="        + encode_URI(am_form.melee_ar2.value);
+  am_url += "&melee_ar3="        + encode_URI(am_form.melee_ar3.value);
+  am_url += "&melee_ar4="        + encode_URI(am_form.melee_ar4.value);
+  am_url += "&melee_ar5="        + encode_URI(am_form.melee_ar5.value);
   am_url += "&melee_damage="     + encode_URI(am_form.melee_damage.value);
   am_url += "&melee_type="       + encode_URI(am_form.melee_type.value);
   am_url += "&melee_flag="       + encode_URI(am_form.melee_flag.value);
@@ -98,7 +103,7 @@ $(document).ready( function() {
     autoOpen: false,
 
     modal: true,
-    height: 375,
+    height: 400,
     width:  600,
     dialogClass: 'dialogWithDropShadow',
     close: function(event, ui) {
@@ -192,73 +197,9 @@ $(document).ready( function() {
 
 <input type="hidden" name="id" value="0">
 
-<table class="ui-styled-table" width="100%">
-<tr>
-  <td class="ui-widget-content">Class <select name="melee_class">
 <?php
-  $q_string  = "select class_id,class_name ";
-  $q_string .= "from class ";
-  $q_string .= "left join subjects on subjects.sub_id = class.class_subjectid ";
-  $q_string .= "where sub_name = \"Melee\" ";
-  $q_string .= "order by class_name ";
-  $q_class = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-  while ($a_class = mysql_fetch_array($q_class)) {
-    print "<option value=\"" . $a_class['class_id'] . "\">" . $a_class['class_name'] . "</option>\n";
-  }
+include('add.melee.dialog.php');
 ?>
-</select></td>
-</tr>
-<tr>
-  <td class="ui-widget-content">Name <input type="text" name="melee_name" size="20"></td>
-</tr>
-<tr>
-  <td class="ui-widget-content">ACC <input type="text" name="melee_acc" size="3"></td>
-</tr>
-<tr>
-  <td class="ui-widget-content">Reach <input type="text" name="melee_reach" size="3"></td>
-</tr>
-<tr>
-  <td class="ui-widget-content">Damage <input type="text" name="melee_damage" size="3"><input type="text" name="melee_type" size="3">(<input type="text" name="melee_flag" size="3">) Use Strength? <input type="checkbox" name="melee_strength"></td>
-</tr>
-<tr>
-  <td class="ui-widget-content">AP <input type="text" name="melee_ap" size="3"></td>
-</tr>
-<tr>
-  <td class="ui-widget-content">Avail <input type="text" name="melee_avail" size="3"><input type="text" name="melee_perm" size="3"> Base Time <input type="text" name="melee_basetime" size="6"> Duration <select name="melee_duration">
-<option value="0">Unset</option>
-<?php
-  $q_string  = "select dur_id,dur_name ";
-  $q_string .= "from duration ";
-  $q_string .= "order by dur_id ";
-  $q_duration = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-  while ($a_duration = mysql_fetch_array($q_duration)) {
-    print "<option value=\"" . $a_duration['dur_id'] . "\">" . $a_duration['dur_name'] . "</option>\n";
-  }
-?>
-</select> (sr3)</td>
-</tr>
-<tr>
-  <td class="ui-widget-content">Street Index <input type="text" name="melee_index" size="6"> (sr3)</td>
-</tr>
-</tr>
-<tr>
-  <td class="ui-widget-content">Cost <input type="text" name="melee_cost" size="3"></td>
-</tr>
-<tr>
-  <td class="ui-widget-content">Book  <select name="melee_book">
-<?php
-  $q_string  = "select ver_id,ver_short ";
-  $q_string .= "from versions ";
-  $q_string .= "where ver_admin = 1 ";
-  $q_string .= "order by ver_short ";
-  $q_versions = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
-  while ($a_versions = mysql_fetch_array($q_versions)) {
-    print "<option value=\"" . $a_versions['ver_id'] . "\">" . $a_versions['ver_short'] . "</option>\n";
-  }
-?>
-</select>: <input type="text" name="melee_page" size="3"></td>
-</tr>
-</table>
 
 </form>
 
