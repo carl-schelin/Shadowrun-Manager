118228580a28a353a84405c3a6e6fa81cc3e1f72
Carl Schelin <cschelin@internal.pri>
2023-07-07 02:47:50 +0000

    Fixed the legth of the totals row

--- edit/firearms.mysql.php
+++ edit/firearms.mysql.php
@@ -439,7 +439,7 @@
           }
         }
         $output .= "<tr>\n";
-        $output .= "  <td class=\"ui-widget-content\" colspan=\"13\">Total Cost: " . return_Cost($costtotal) . "</td>\n";
+        $output .= "  <td class=\"ui-widget-content\" colspan=\"12\">Total Cost: " . return_Cost($costtotal) . "</td>\n";
         $output .= "</tr>\n";
       } else {
         $output .= "<tr>\n";
