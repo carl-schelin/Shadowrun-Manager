a8a5a36282068d65c0d5427c886630b5e39eee26
Carl Schelin <cschelin@internal.pri>
2023-07-08 00:39:47 +0000

    removed the subjecs table check

--- edit/armor.del.php
+++ edit/armor.del.php
@@ -30,9 +30,7 @@
 
       $q_string  = "delete ";
       $q_string .= "from r_accessory ";
-      $q_string .= "left join accessory on accessory.acc_id = r_accessory.r_acc_number ";
-      $q_string .= "left join subjects on subjects.sub_id = accessory.acc_type ";
-      $q_string .= "where sub_name = \"Clothing and Armor\" and r_acc_character = " . $a_r_armor['r_arm_character'] . " and r_acc_parentid = " . $formVars['id'] . " ";
+      $q_string .= "where r_acc_character = " . $a_r_armor['r_arm_character'] . " and r_acc_parentid = " . $formVars['id'] . " ";
       $result = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
 
       $q_string  = "delete ";
