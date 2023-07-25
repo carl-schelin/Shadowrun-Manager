cf1ad9b3186981577a7bd61ad556967c953fd82b
Carl Schelin <cschelin@internal.pri>
2023-07-25 21:28:17 +0000

    fixed the autodel script

---
+++ edit/vehauto.del.php
@@ -0,0 +1,36 @@
+<?php
+# Script: vehauto.del.php
+# Owner: Carl Schelin
+# Coding Standard 3.0 Applied
+# See: https://incowk01/makers/index.php/Coding_Standards
+# Description: Delete association entries
+
+  header('Content-Type: text/javascript');
+
+  include('settings.php');
+  $called = 'yes';
+  include($Loginpath . '/check.php');
+  include($Sitepath . '/function.php');
+
+  if (isset($_SESSION['username'])) {
+    $package = "vehauto.del.php";
+    $formVars['id'] = 0;
+    if (isset($_GET['id'])) {
+      $formVars['id'] = clean($_GET['id'], 10);
+    }
+
+    if (check_userlevel(3)) {
+      logaccess($_SESSION['username'], $package, "Removing parentid " . $formVars['id'] . " from r_gear");
+
+      $q_string  = "update  ";
+      $q_string .= "r_gear ";
+      $q_string .= "set r_gear_parentid = 0 ";
+      $q_string .= "where r_gear_id= " . $formVars['id'];
+      $insert = mysql_query($q_string) or die(header("Location: " . $Siteroot . "/error.php?script=" . $package . "&error=" . $q_string . "&mysql=" . mysql_error()));
+
+      print "alert('Autosoft association removed.');\n";
+    } else {
+      logaccess($_SESSION['username'], $package, "Access denied");
+    }
+  }
+?>
